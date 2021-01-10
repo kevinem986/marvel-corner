976c32790c8911b9fea5799e6a3a458b7713c1d1
Kevin Hernaldo Almendarez Caldera <kevinem986@hotmail.com>
2021-01-10 05:06:58 +0100

    fixed bug images headernavbar

--- src/components/HeaderNavbar/index.js
+++ src/components/HeaderNavbar/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
-import Image from 'react-bootstrap/Image';
+// import Image from 'react-bootstrap/Image';
 
 const HeaderNavbar = () => {
   return (
